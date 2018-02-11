f80a91f0ece66bf1370539df13d92fa9e6eb7c84
Dipiert <damian_rott2@hotmail.com>
2018-02-11 13:10:56 -0300

    add cap3lesson5 Haskell exercises

---
+++ capitulo_3_funcional/leccion_5_aplicacion_parcial_y_orden_superior/ejercicio_10_tipos_y_orden.hs
@@ -0,0 +1,3 @@
+-- https://mumuki.io/exercises/1607-programacion-funcional-aplicacion-parcial-y-orden-superior-tipos-y-orden-superior
+
+saludoDoble:: (String -> String) -> String -> String -> String
---
+++ capitulo_3_funcional/leccion_5_aplicacion_parcial_y_orden_superior/ejercicio_12_juntando_todo.hs
@@ -0,0 +1,4 @@
+-- https://mumuki.io/exercises/1609-programacion-funcional-aplicacion-parcial-y-orden-superior-juntando-todo
+
+esNoVacio :: String -> Bool
+esNoVacio = (>0).length
